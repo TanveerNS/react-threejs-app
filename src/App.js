1cad02429646492335a723866eac3a7f9d438ab9
tanveerns <tanveeralam229@gmail.com>
2022-09-23 22:56:40 +0530

    box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Box from './pages/Box';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="box" element={<Box />}/>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Canvas } from '@react-three/fiber'
+import Box from '../../components/Box'
 const Home = () => {
   return (
+    <>
     <div className='text-3xl font-bold'>ThreeJS App</div>
+    <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>
+    </>
   )
 }
 
